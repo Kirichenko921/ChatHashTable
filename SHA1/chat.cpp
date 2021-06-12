f1970fd4d112671d97800a94801ab613af7fff1d
Kirichenko Alexander <050780@inbox.ru>
2021-06-12 16:11:09 +0300

    создана регистрация и удаление логина пользователя

---
+++ SHA1/chat.cpp
@@ -0,0 +1,100 @@
+﻿#include "chat.h"
+
+
+HashTable::HashTable() {
+    _countLogin = 0;
+    _memSize = 8;
+    _arrayData = new Pair[_memSize];
+ }
+HashTable::~HashTable() {
+    delete[] _arrayData;
+}
+void HashTable::registration(char login[], char password[], int passwordLenght)
+{
+    uint* digest = sha1(password, passwordLenght);
+    registrationInner(login, digest);
+}
+
+void  HashTable::registrationInner(char login[LOGINLENGTH], uint* digest)
+{
+    int index = -1, i = 0;
+
+    for (; i < _memSize; i++)
+    {
+        index = hashFunction(login, i * i);
+        if (_arrayData[index]._status == enPairStatus::free)
+        {
+            // найдена пустая ячейка, занимаем ее
+            break;
+        }
+    }
+
+    if (i >= _memSize)
+    {
+        resize();
+        registrationInner(login, digest);
+    }
+
+    else
+    {
+        _arrayData[index] = Pair(login, digest);
+        _countLogin++;
+    }
+
+}
+
+void HashTable::deletingRegistered(char login[LOGINLENGTH])
+{
+    int index, i = 0;
+    for (; i < _memSize; i++) {
+        index = hashFunction(login, i * i);
+        if (_arrayData[index]._status == enPairStatus::deleted ||_arrayData[index]._status == enPairStatus::free)
+            return;
+        else if (_arrayData[index]._status == enPairStatus::engaged && !memcmp(login,_arrayData[index]._login, LOGINLENGTH))
+                       break;
+    }
+    if (i >= _memSize) 
+        return;
+
+    _arrayData[index]._status = enPairStatus::deleted;
+}
+int HashTable::hashFunction(char login[LOGINLENGTH], int step)
+{
+    // вычисляем индекс
+    int sum = 0, i = 0;
+    for (; i < LOGINLENGTH; i++)
+    {
+        sum += login[i];
+    }
+   
+    return (sum % _memSize + step) % _memSize;
+}
+void HashTable::resize() {
+
+    Pair* arraySave = new Pair[_memSize]; // запоминаем старый массив
+    int oldSize = _memSize;
+       for (int i = 0; i < _memSize; ++i)
+        arraySave[i] = _arrayData[i];
+    _memSize *= 2;  // увеличиваем размер в два раза  
+    _countLogin = 0; // обнуляем количество элементов
+    _arrayData = new Pair[_memSize]; // выделяем новую память
+    
+    for (int i = 0; i < oldSize; i++) 
+    {
+        Pair& oldData = arraySave[i];
+        if (oldData._status == enPairStatus::engaged)
+        {     uint* shaHashRewritable = new uint[SHA1HASHLENGTHUINTS];
+        memcpy(shaHashRewritable, oldData._pass_sha1_hash, SHA1HASHLENGTHBYTES);
+
+        registrationInner(oldData._login, shaHashRewritable);
+         }
+    // чистим за собой
+    delete[] arraySave;
+}
+   
+   
+  
+
+  
+
+   
---
+++ SHA1/chat.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+#include "sha1.h"
+
+#include <string.h>
+
+#define LOGINLENGTH  10  // Зафикируем длину имени
+#define SIZE 10
+
+
+class HashTable { // хэш таблица
+public:
+
+	HashTable();
+	~HashTable();
+	void registration(char login[LOGINLENGTH], char password[], int passwordLenght);
+	void registrationInner(char login[LOGINLENGTH], uint* digest);
+	void deletingRegistered(char login[LOGINLENGTH]);
+private:
+
+	enum enPairStatus {
+		free,
+		engaged,
+		deleted
+	};
+
+	struct Pair { // пара логин-пароль
+
+		Pair() : _login(""), _pass_sha1_hash(0), _status(enPairStatus::free)
+		{}
+		~Pair()
+		{
+			if (_pass_sha1_hash != 0)
+				delete[] _pass_sha1_hash;
+		}
+		Pair(char login[LOGINLENGTH], uint* sha1)
+		{
+			memcpy(_login, login, LOGINLENGTH);
+			_pass_sha1_hash = sha1;
+			_status = enPairStatus::engaged;
+		}
+		Pair& operator = (const Pair& other)
+		{
+			memcpy(_login, other._login, LOGINLENGTH);
+
+			if (_pass_sha1_hash != 0)
+				delete[] _pass_sha1_hash;
+			_pass_sha1_hash = new uint[SHA1HASHLENGTHUINTS];
+
+			memcpy(_pass_sha1_hash, other._pass_sha1_hash, SHA1HASHLENGTHBYTES);
+
+			_status = other._status;
+
+			return *this;
+		}
+
+		bool operator == (const Pair& other) {
+			return  _status == other._status &&
+				(_status != enPairStatus::engaged || (!memcmp(_pass_sha1_hash, other._pass_sha1_hash, SHA1HASHLENGTHBYTES) && !strcmp(_login, other._login)));
+		}
+
+		char _login[LOGINLENGTH];
+		uint* _pass_sha1_hash;
+
+		enPairStatus _status;
+	};
+
+	void resize();
+	int hashFunction(char login[LOGINLENGTH], int step);
+
+
+	Pair* _arrayData;
+	int _memSize;
+	int _countLogin;
+};
